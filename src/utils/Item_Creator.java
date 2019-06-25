463872be88ea6f32ef41b33eb86002f79549e10b
IsqMatheus <matheus2043@gmail.com>
2019-06-24 23:30:52 -0300

    Protótipo de gerador de itens aperfeiçoado

--- src/utils/Item_Creator.java
+++ src/utils/Item_Creator.java
@@ -13,62 +13,63 @@ import item.model.Item;
 import java.util.Random;
 
 public class Item_Creator{
+    private Random r;
+    private String[] levels;
 
-    public static Item getRandom(){
-        return get(new Random().nextInt(8));
+    public Item_Creator() {
+        this.r = new Random();
+        this.levels = new String[]{"pessimo","ruim", "bom", "ótimo", "lendário"};
     }
 
-    public static Item get(int category){
+    public Item getRandom(){
+        return get(r.nextInt(8), r.nextInt(5), "" + Generator.get());
+    }
+
+    public Item get(int category, int lvl, String owner){
         switch (category){
-            case 0: return createCoinBag();
-            case 1: return createHeal();
-            case 2: return createRing();
-            case 3: return createWeapon();
-            case 4: return createArm_Piece();
-            case 5: return createChest_Piece();
-            case 6: return createHelmet();
-            case 7: return createLeg_Piece();
+            case 0: return createCoinBag(lvl, owner);
+            case 1: return createHeal(lvl, owner);
+            case 2: return createRing(lvl, owner);
+            case 3: return createWeapon(lvl, owner);
+            case 4: return createArm_Piece(lvl, owner);
+            case 5: return createChest_Piece(lvl, owner);
+            case 6: return createHelmet(lvl, owner);
+            case 7: return createLeg_Piece(lvl, owner);
             default: return null;
         }
 
     }
 
-    public static Item createCoinBag(){
-        return new CoinBag("Saco de moedas velho", new Random().nextInt(95) + 5);
+    public Item createCoinBag(int lvl, String owner){
+        return new CoinBag("Moedas do " + owner, (r.nextInt(15) + 5)*(lvl+1));
     }
 
-    public static Item createHeal(){
-        return new Heal("Poção velha", 1, new Random().nextInt(4) + 1);
+    public Item createHeal(int lvl, String owner){
+        return new Heal("Poção " + levels[lvl], 1, (r.nextInt(2) + 1)*(lvl+1));
     }
 
-    public static Item createRing() {
-        Random r = new Random();
-        return new Ring("Anel de " + Generator.get(), Bonus.Type.values()[r.nextInt(3)], r.nextInt(4) + 1);
+    public Item createRing(int lvl, String owner) {
+        return new Ring("Anel " + levels[lvl] + " de " + owner, Bonus.Type.values()[r.nextInt(3)], (r.nextInt(2) + 1)*(lvl+1));
     }
 
-    public static Item createWeapon(){
-        Random r = new Random();
-        return new Weapon("Arma de " + Generator.get(), r.nextInt(3) + 1, r.nextInt(4) + 1);
+    public Item createWeapon(int lvl, String owner){
+        return new Weapon("Arma " + levels[lvl] + "de " + owner, r.nextInt(3) + 1, (r.nextInt(1) + 1)*(lvl+1));
     }
 
-    public static Item createArm_Piece(){
-        Random r = new Random();
-        return new Arm_Piece("Manoplas de " + Generator.get(), r.nextInt(2) + 1, r.nextInt(4) + 1, r.nextInt(4) + 1, Bonus.Type.values()[r.nextInt(3)]);
+    public Item createArm_Piece(int lvl, String owner){
+        return new Arm_Piece("Manoplas " + levels[lvl] + " de " + owner, r.nextInt(2) + 1, (r.nextInt(1) + 1)*(lvl+1), (r.nextInt(1) + 1)*(lvl+1), Bonus.Type.values()[r.nextInt(3)]);
     }
 
-    public static Item createChest_Piece(){
-        Random r = new Random();
-        return new Chest_Piece("Peitoral de " + Generator.get(), r.nextInt(4) + 1, r.nextInt(9) + 1, r.nextInt(9) + 1, Bonus.Type.values()[r.nextInt(3)]);
+    public Item createChest_Piece(int lvl, String owner){
+        return new Chest_Piece("Peitoral " + levels[lvl] + " de " + owner, r.nextInt(4) + 1, (r.nextInt(2) + 1)*(lvl+1), (r.nextInt(2) + 1)*(lvl+1), Bonus.Type.values()[r.nextInt(3)]);
     }
 
-    public static Item createHelmet(){
-        Random r = new Random();
-        return new Helmet("Capacete de " + Generator.get(), r.nextInt(2) + 1, r.nextInt(4) + 1, r.nextInt(4) + 1, Bonus.Type.values()[r.nextInt(3)]);
+    public Item createHelmet(int lvl, String owner){
+        return new Helmet("Capacete " + levels[lvl] + " de " + owner, r.nextInt(2) + 1, (r.nextInt(1) + 1)*(lvl+1), (r.nextInt(1) + 1)*(lvl+1), Bonus.Type.values()[r.nextInt(3)]);
     }
 
-    public static Item createLeg_Piece(){
-        Random r = new Random();
-        return new Leg_Piece("Pernal de " + Generator.get(), r.nextInt(3) + 1, r.nextInt(4) + 1, r.nextInt(4) + 1, Bonus.Type.values()[r.nextInt(3)]);
+    public Item createLeg_Piece(int lvl, String owner){
+        return new Leg_Piece("Pernal " + levels[lvl] + " de " + owner, r.nextInt(3) + 1, (r.nextInt(1) + 1)*(lvl+1), (r.nextInt(1) + 1)*(lvl+1), Bonus.Type.values()[r.nextInt(3)]);
     }
 
 }
